d2523a3d7e94e8006a280314db6843c26be7306a
bbartels <benjamin@bartels.dev>
2025-06-24 02:07:45 +0100

    Fixes

---
+++ test-instructions.sh
@@ -0,0 +1,10 @@
+
+ln -s ~/Documents/GitHub/efcore/.dotnet/sdk/Microsoft.NETCore.App/10.0.100-preview.6.25315.102/ /home/bbartels/.dotnet/sdk/Microsoft.NETCore.App/10.0.100-preview.6.25315.102
+ln -s ~/Documents/GitHub/efcore/.dotnet/shared/Microsoft.NETCore.App/10.0.0-preview.6.25315.102/ /home/bbartels/.dotnet/shared/Microsoft.NETCore.App/10.0.0-preview.6.25315.102
+ln -s ~/Documents/GitHub/efcore/.dotnet/shared/Microsoft.NETCore.App/10.0.0-preview.7.25320.118/ /home/bbartels/.dotnet/shared/Microsoft.NETCore.App/10.0.0-preview.7.25320.118
+ln -s ~/Documents/GitHub/efcore/.dotnet/shared/Microsoft.AspNetCore.App/10.0.0-preview.6.25315.102/ /home/bbartels/.dotnet/shared/Microsoft.AspNetCore.App/10.0.0-preview.6.25315.102
+
+rm /home/bbartels/.dotnet/sdk/Microsoft.NETCore.App/10.0.100-preview.6.25315.102
+rm /home/bbartels/.dotnet/shared/Microsoft.NETCore.App/10.0.0-preview.6.25315.102
+rm /home/bbartels/.dotnet/shared/Microsoft.NETCore.App/10.0.0-preview.7.25320.118
+rm /home/bbartels/.dotnet/shared/Microsoft.AspNetCore.App/10.0.0-preview.6.25315.102
